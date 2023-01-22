0e94429ac7f50e14461155470a435788a22b444a
DaniellePashayan <87950814+DaniellePashayan@users.noreply.github.com>
2023-01-22 10:40:44 -0500

    exported LE classes so they can be imported when predicting

--- references/ML Training/training.py
+++ references/ML Training/training.py
@@ -3,74 +3,24 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-import joblib
-from os.path import exists
+import numpy as np
 
 process = 'NonCodify'
-print(exists(f'../ML Training/training data/{process}_training_data.csv'))
 inputs = pd.read_csv(f'../ML Training/training data/{process}_training_data.csv')
-
-
-features = ['PAYER', 'Process', 'SHS Status', 'PIC Desc', 'Form Letter', 'Records', 'ACK']
 inputs.fillna('0', inplace = True)
 
 le_payer = LabelEncoder()
 le_coding_tool = LabelEncoder()
 le_shs_status = LabelEncoder()
 le_pic_desc = LabelEncoder()
-le_category = LabelEncoder()
 le_status = LabelEncoder()
-le_comment = LabelEncoder()
-
-def Category(training_data, process):
-
-    training_data['Form Letter'] = training_data['Form Letter'].astype('int64')
-    if process == 'Bundling':
-        training_data['Proofs'] = training_data['Proofs'].astype('int64')
-    elif process == 'medicalrecords':
-        training_data['Records'] = training_data['Records'].astype('int64')
-    training_data['ACK'] = training_data['ACK'].astype('int64')
-
-    # converts the values into numbers
-    training_data['payer_n'] = le_payer.fit_transform(training_data['PAYER'])
-    if process == 'Bundling':
-        training_data['coding_tool_n'] = le_coding_tool.fit_transform(training_data['Coding Tool'])
-    elif process == 'medicalrecords':
-        training_data['process_n'] = le_coding_tool.fit_transform(training_data['Process'])
-    training_data['shs_status_n'] = le_shs_status.fit_transform(training_data['SHS Status'])
-    training_data['pic_desc_n'] = le_pic_desc.fit_transform(training_data['PIC Desc'])
-
-    training = training_data.copy()
-    training['category_n'] = le_category.fit_transform(training_data['Category'])
-
-    training_data = training_data.drop(
-        columns = ['PAYER', 'Process', 'SHS Status', 'PIC Desc', 'Category', 'Status', 'Comment'], axis = 'columns')
-    training = training.drop(columns = ['PAYER', 'Process', 'SHS Status', 'PIC Desc', 'Status', 'Comment'],
-                             axis = 'columns')
-
-    # sets the keys for the data you are feeding it
-
-    model = DecisionTreeClassifier()
-
-    X = training_data
-    y = pd.DataFrame(training['category_n'])
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
-
-    model.fit(X_train, y_train)
-    predictions = model.predict(X_test)
-
-    accuracy = accuracy_score(y_test, predictions)
-    print(accuracy)
-
-    joblib.dump(model, f'../ML Training/models/{process}_audit_category.joblib')
-
 
 def Status(training_data, process):
     training_data['Form Letter'] = training_data['Form Letter'].astype('int64')
     training_data['Proofs'] = training_data['Proofs'].astype('int64')
     training_data['ACK'] = training_data['ACK'].astype('int64')
 
+    df = training_data.copy()
     # converts the values into numbers
     training_data['payer_n'] = le_payer.fit_transform(training_data['PAYER'])
     if process == 'NonCodify':
@@ -79,12 +29,17 @@ def Status(training_data, process):
         training_data['process_n'] = le_coding_tool.fit_transform(training_data['Process'])
     training_data['shs_status_n'] = le_shs_status.fit_transform(training_data['SHS Status'])
     training_data['pic_desc_n'] = le_pic_desc.fit_transform(training_data['PIC Desc'])
+    classes = le_pic_desc.classes_
 
     training = training_data.copy()
-    test = pd.DataFrame()
-    test['Status'] = training_data['Status'].unique()
-    test['status_n'] = le_status.fit_transform(test['Status'])
     training['status_n'] = le_status.fit_transform(training['Status'])
+    print(le_status.classes_)
+    np.save('status_classes.npy', le_status.classes_)
+    np.save('payer_classes.npy', le_payer.classes_)
+    np.save('coding_tool_classes.npy', le_coding_tool.classes_)
+    np.save('shs_status_classes.npy', le_shs_status.classes_)
+    np.save('pic_desc_classes.npy', le_pic_desc.classes_)
+
 
     training_data = training_data.drop(columns = ['PAYER', 'Coding Tool', 'SHS Status', 'PIC Desc',
                                                   'Category', 'Status', 'Comment'], axis = 'columns')
@@ -100,56 +55,16 @@ def Status(training_data, process):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
 
     model.fit(X_train, y_train)
-    predictions = model.predict(X_test)
+    # predictions = model.predict(X_test)
+    # X_test['Status'] = le_status.inverse_transform(model.predict(X_test))
+    df['Status2'] = le_status.inverse_transform(model.predict(X))
+    # X['xyz'] = model.predict(X)
     status = le_status.inverse_transform(predictions)
 
-    accuracy = accuracy_score(y_test, predictions)
-    print(accuracy)
+    # accuracy = accuracy_score(y_test, predictions)
+    print('f')
 
-    joblib.dump(model, f'../ML Training/models/{process}_audit_status.joblib')
+    # joblib.dump(model, f'../ML Training/models/{process}_audit_status.joblib')
 
-
-def Comment(training_data, process):
-
-    training_data['Form Letter'] = training_data['Form Letter'].astype('int64')
-    if process == 'Bundling':
-        training_data['Proofs'] = training_data['Proofs'].astype('int64')
-    elif process == 'medicalrecords':
-        training_data['Records'] = training_data['Records'].astype('int64')
-    training_data['ACK'] = training_data['ACK'].astype('int64')
-
-    # converts the values into numbers
-    # converts the values into numbers
-    training_data['payer_n'] = le_payer.fit_transform(training_data['PAYER'])
-    if process == 'Bundling':
-        training_data['coding_tool_n'] = le_coding_tool.fit_transform(training_data['Coding Tool'])
-    elif process == 'medicalrecords':
-        training_data['process_n'] = le_coding_tool.fit_transform(training_data['Process'])
-    training_data['shs_status_n'] = le_shs_status.fit_transform(training_data['SHS Status'])
-    training_data['pic_desc_n'] = le_pic_desc.fit_transform(training_data['PIC Desc'])
-
-    training = training_data.copy()
-    training['comment_n'] = le_comment.fit_transform(training['Comment'])
-
-    training_data = training_data.drop(columns = ['PAYER', 'Process', 'SHS Status', 'PIC Desc',
-                                                  'Category', 'Status', 'Comment'], axis = 'columns')
-    training = training.drop(columns = ['PAYER', 'Process', 'SHS Status', 'PIC Desc', 'Category', 'Status'], axis = 'columns')
-
-    # sets the keys for the data you are feeding it
-    model = DecisionTreeClassifier()
-
-    X = training_data
-    y = pd.DataFrame(training['comment_n'])
-
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
-
-    model.fit(X_train, y_train)
-    predictions = model.predict(X_test)
-
-    accuracy = accuracy_score(y_test, predictions)
-    print(accuracy)
-
-    joblib.dump(model, f'../ML Training/models/{process}_audit_comment.joblib')
 
 Status(inputs, 'NonCodify')
--- src/function.py
+++ src/function.py
@@ -1,9 +1,11 @@
 from configparser import ConfigParser
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
+import numpy as np
+import joblib
 
 
-def merge(input, output, shs, process):
+def merge_format(input, output, shs, process):
     config_path = 'config.ini'
     config = ConfigParser()
     config.read(config_path)
@@ -29,23 +31,51 @@ def merge(input, output, shs, process):
                   left_on = ['INVNUM', 'File Date', 'Coding Tool'],
                   right_on = ['Invoice', 'File Date', 'Process'])
     df.drop(columns = ['SHS Name', 'Invoice_x', 'Name', 'Bot Type', 'Bot Name', 'Invoice_y', 'Process'], inplace = True)
+
+    inputs = ''
+    outputs = ''
+    shs = ''
+    pic_desc = ''
+    pics = ''
+
+    df = df.fillna('0').astype('str')
     return df
 
-def preprocess(data):
+def preprocess(df):
     le_payer = LabelEncoder()
     le_coding_tool = LabelEncoder()
     le_shs_status = LabelEncoder()
     le_pic_desc = LabelEncoder()
-    le_category = LabelEncoder()
     le_status = LabelEncoder()
-    le_comment = LabelEncoder()
 
-    df = data[['ACK', 'Form Letter', 'Proofs']]
-    df['ACK'] = df['ACK'].astype('int64')
-    df['Form Letter'] = df['Form Letter'].astype('int64')
-    df['Proofs'] = df['Proofs'].astype('int64')
+    data = df[['ACK', 'Form Letter', 'Proofs']]
+    data['ACK'] = data['ACK'].astype('int64')
+    data['Form Letter'] = data['Form Letter'].astype('int64')
+    data['Proofs'] = data['Proofs'].astype('int64')
 
-    df['payer_n'] = le_payer.fit_transform(data['PAYER'])
-    df['coding_tool_n'] = le_coding_tool.fit_transform(data['Coding Tool'])
-    df['shs_status_n'] = le_shs_status.fit_transform(data['RetrievalStatus'])
-    df['pic_desc_n'] = le_shs_status.fit_transform(data['Name'])
+    model = joblib.load(
+        'C:/Users/dpashayan/PycharmProjects/Monthly_Audit/references/ML Training/models/noncodify_audit_status.joblib')
+
+    le_status.classes_ = np.load(
+        'C:/Users/dpashayan/PycharmProjects/Monthly_Audit/references/ML Training/status_classes.npy',
+        allow_pickle = True)
+    le_pic_desc.classes_ = np.load(
+        'C:/Users/dpashayan/PycharmProjects/Monthly_Audit/references/ML Training/pic_desc_classes'
+        '.npy', allow_pickle = True)
+    le_coding_tool.classes_ = np.load(
+        'C:/Users/dpashayan/PycharmProjects/Monthly_Audit/references/ML Training/coding_tool_classes.npy',
+        allow_pickle = True)
+    le_shs_status.classes_ = np.load(
+        'C:/Users/dpashayan/PycharmProjects/Monthly_Audit/references/ML Training/shs_status_classes.npy',
+        allow_pickle = True)
+    le_payer.classes_ = np.load(
+        'C:/Users/dpashayan/PycharmProjects/Monthly_Audit/references/ML Training/payer_classes.npy',
+        allow_pickle = True)
+
+    data['payer_n'] = le_payer.transform(df['PAYER'])
+    data['coding_tool_n'] = le_coding_tool.transform(df['Coding Tool'])
+    data['shs_status_n'] = le_shs_status.transform(df['RetrievalStatus'])
+    data['pic_desc_n'] = le_pic_desc.transform(df['Name'])
+
+    df['Status'] = le_status.inverse_transform(model.predict(data))
+    return df
